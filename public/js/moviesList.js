42a04f2da31a6e238246bfc7976e5e6829fdf6fd
flaviadanielareisenauer <flaviareisenauer@gmail.com>
2022-01-06 23:05:55 -0300

    agregando eventos

--- public/js/moviesList.js
+++ public/js/moviesList.js
@@ -1,17 +1,15 @@
-window.onload = function(){
-    let body = document.querySelector('body');
-    let moviesListTitulo = document.querySelector('.moviesListTitulo');
+window.onload = function () {
+  let body = document.querySelector("body");
+  let moviesListTitulo = document.querySelector(".moviesListTitulo");
+  let logo = document.querySelector(".logoDH");
 
-    let modo = confirm('Desea modo oscuro');
-    if(modo){
-        body.style.backgroundColor = '#7f7f7f'
-        body.classList.add('fondoMoviesList');
-    }
-    
-    console.log(body);
-    moviesListTitulo.innerHTML = 'LISTADO DE PELÍCULAS';
-    moviesListTitulo.style.color ='white';
-    moviesListTitulo.style.backgroundColor = 'teal';
-    moviesListTitulo.style.padding = '20px';
-
-}
+  logo.addEventListener("mouseover", function () {
+    body.style.backgroundColor = "#7f7f7f";
+    body.classList.add("fondoMoviesList");
+    moviesListTitulo.innerHTML = "LISTADO DE PELÍCULAS";
+    moviesListTitulo.style.color = "white";
+    moviesListTitulo.style.backgroundColor = "teal";
+    moviesListTitulo.style.padding = "20px";
+  });
+  console.log(window);
+};
